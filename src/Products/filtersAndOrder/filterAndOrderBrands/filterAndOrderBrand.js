2434d294fb78080423e9e95de9e7fd91f62f0eea
Eukaryae <cami.igsa@gmail.com>
2024-05-14 18:19:38 -0400

    fix: filtersbrand/?nameBrand=

--- src/Products/filtersAndOrder/filterAndOrderBrands/filterAndOrderBrand.js
+++ src/Products/filtersAndOrder/filterAndOrderBrands/filterAndOrderBrand.js
@@ -3,12 +3,13 @@ const {Op} = require('sequelize');
 
 const filterAndOrderBrand = async (req, res) => {
     const { nameBrand, orderBy, orderDirection } = req.query;
-    let { page, limit } = req.query;
-    const offset = (page - 1) * limit;
+    // let { page, limit } = req.query;
+    // const offset = (page - 1) * limit;
     const where = {};
 
     try {
         if (nameBrand) {
+            // console.log(nameBrand)
             where.nameBrand = { [Op.iLike]: `%${nameBrand}%` }
         }
 
@@ -30,8 +31,8 @@ const filterAndOrderBrand = async (req, res) => {
 
         const resultFiltersAndOrder = await EntityBrand.findAndCountAll({
             where: { ...where },
-            offset,
-            limit,
+            // offset,
+            // limit,
             order: order.length > 0 ? order : undefined,
             include:[{
                 model: CharacteristicsProducts,
@@ -65,7 +66,7 @@ const filterAndOrderBrand = async (req, res) => {
         // });
 
         if (!productsFilteredByBrand || productsFilteredByBrand.length < 1) {
-            return res.status(200).send('No existen coincidencias.')
+            return res.status(400).send('No existen coincidencias.')
         }
         
         return res.status(200).json(productsFilteredByBrand)
