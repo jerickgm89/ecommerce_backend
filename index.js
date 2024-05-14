7ab8bc641546dadd65e972f16ba0762ede83ad2e
JErickDev <67766347+jerickgm89@users.noreply.github.com>
2024-05-14 12:19:41 -0500

    Revert "Fix: optional paginate getAllProducts"

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
+
 const server = require('./src/server');
-const {conn} = require('./src/db.js');
-const PORT = process.env.PORT || 3001;
-const { initializeData } = require('./src/config/databaseInit')
 
-conn.sync({ force: true }).then(async () => {
-  try {
-    await initializeData(); // aqui se llama a la funcion initializeData que AUTOMATICAMENTE CARGUE PRODUCTOS AL LEVANTAR EL SERVER 
-    console.log('Initial data loaded successfully');
+const PORT = 3001;
 
+try {
     server.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT}`);
+        console.log(`Server listening on port ${PORT}`)
     });
-
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-});
+} catch (error) {
+    console.log(error)
+}
