1c481eb3b082dda57a903aa9d30e2135abbbc8a4
Anthony Joel <ajds.joel1995@gmail.com>
2024-05-19 17:41:26 -0500

    payment

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+// src/config/config.js
+module.exports = {
+    mercadoPago: {
+      accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN,  // Usando variables de entorno
+      publicKey: process.env.MERCADO_PAGO_PUBLIC_KEY,      // Usando variables de entorno
+    },
+  };
+  
