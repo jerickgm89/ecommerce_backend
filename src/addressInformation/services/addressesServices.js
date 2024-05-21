f082fde63a3f8b474112a860029ecb9780e70ec3
Eukaryae <cami.igsa@gmail.com>
2024-05-21 12:38:17 -0400

    fix address filter information

--- src/addressInformation/services/addressesServices.js
+++ src/addressInformation/services/addressesServices.js
@@ -1,24 +1,32 @@
 const formatedResponse = (info) => {
-    const toFormat = Object.keys(info)[0].toLowerCase()
-    const response = toFormat[0].toUpperCase() + toFormat.slice(1)
-    return response
+    const arrayList = typeof info !== "string" ? Object.keys(info) : info
+    const toFormat = Array.isArray(arrayList) 
+    ? arrayList.map(city => city[0] + city.slice(1).toLowerCase()) 
+    : arrayList[0] + arrayList.slice(1).toLowerCase()
+    return toFormat
 } 
 const getPostalCodeServices = ({ province, city }, fullListPostalCode) => {
+    
+    // Retorna la ciudad seleccionada según selecciones de ciudad y provincia
     if( province && city ){
         const filterProvinceAndCity = fullListPostalCode[province][city]
-        const citySelected = formatedResponse(filterProvinceAndCity)
-        return citySelected
+        if(Object.keys(filterProvinceAndCity).length) return formatedResponse(city)
+        else throw new Error ("No se encontró coincidencia")
         
     }
+
+    // Retorna el listado de ciudades según la provincia seleccionada
     if( province && !city ){
         const filterProvince = fullListPostalCode[province]
         const citySelected = formatedResponse(filterProvince)
         return citySelected
     }
+
+    // Retorna la ciudad seleccionada
     if ( !province && city ) {
         for(const province in fullListPostalCode){
             const toString = formatedResponse(fullListPostalCode[province][city])
-                return toString
+            if( toString.length ) return formatedResponse(city)
         }
     }
     else {
