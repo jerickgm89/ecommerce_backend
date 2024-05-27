186d0a576f997dcd117a9b85120e1804099f2a03
Eukaryae <cami.igsa@gmail.com>
2024-05-26 23:35:58 -0400

    refact: route discounts by name

--- src/Products/routes/discount/discountRoutes.js
+++ src/Products/routes/discount/discountRoutes.js
@@ -14,7 +14,7 @@ const router = express.Router();
 
 router.post('/both/:idProduct', createDiscountController); // crea un descuento simple // crea un descuento de grupo
 router.get('/product/:idProduct', getDiscountByProductController); // trae todos los descuentos relacionados al producto según idProduct
-router.get('/name/:name', getDiscountByNameController); // busca según nombre de descuento sin importar mayúsculas
+router.get('/both/:name', getDiscountByNameController); // busca según nombre de descuento sin importar mayúsculas
 router.get('/group/', getAllGroupOfDiscountController); // obtiene todos los descuentos de grupo
 router.get('/both/:idDiscount', getDiscountByIdController); // obtiene el descuento según idDiscount
 router.put('/both/:idDiscount', updateDiscountController); //modifica los descuentos de un dscto simple  // modifica los dsctos en grupo
